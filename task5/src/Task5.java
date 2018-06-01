c4658d0d9f87dd62a0d16d0dcafdda8d3e15a2b4
ShurinVitaliy <34574399+ShurinVitaliy@users.noreply.github.com>
2018-06-01 21:20:11 +0400

    senla

--- task5/src/Task5.java
+++ task5/src/Task5.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -7,15 +8,44 @@ import java.util.regex.Pattern;
  * Created by User on 31.05.2018.
  */
 public class Task5 {
+    public static String blank_line(String primaryString){
+        primaryString = primaryString.replace(',',' ');
+        primaryString = primaryString.trim();
+        primaryString = primaryString.replaceAll("[\\s]{2,}", " ");
+        return primaryString;
+    }
+
+    public static List<String> get_words(String blankLine){
+        List<String> words = new ArrayList<String>();
+        boolean flag = true;
+        String currentStrin = "";
+        for (int i=0; i < blankLine.length(); i++){
+            if(!Character.isLetter(blankLine.charAt(i))){
+                words.add(currentStrin);
+                currentStrin = "";
+            }
+            else{
+                currentStrin = currentStrin + blankLine.charAt(i);
+                if(i == blankLine.length()-1)
+                    words.add(currentStrin);
+            }
+        }
+        return words;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String string = in.nextLine();
         String word = in.nextLine();
-        Pattern pattern = Pattern.compile(word);
-        Matcher matcher = pattern.matcher(string);
+        word = blank_line(word);
+        string = blank_line(string);
         int count = 0;
-        while(matcher.find())
-            count++;
+        List<String> words = get_words(string);
+        for (String wordq : words) {
+            if (wordq.equals(word)){
+                count++;}
+        }
         System.out.println(count);
+
     }
 }
