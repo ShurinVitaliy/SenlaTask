9fc42fb14557f73ad330ed26cf40c56e8260ecff
ShurinVitaliy <34574399+ShurinVitaliy@users.noreply.github.com>
2018-06-01 18:08:44 +0400

    senla

--- task4/src/Task4.java
+++ task4/src/Task4.java
@@ -1,12 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * Created by User on 31.05.2018.
+ * Created by User on 01.06.2018.
  */
 public class Task4 {
+    public static String blank_line(String primaryString){
+        primaryString = primaryString.replace(',',' ');
+        primaryString = primaryString.trim();
+        primaryString = primaryString.replaceAll("[\\s]{2,}", " ");
+        return primaryString;
+    }
+
+    public static List<String> get_words(String blankLine){
+    List<String> words = new ArrayList<String>();
+    boolean flag = true;
+    String currentStrin = "";
+    for (int i=0; i < blankLine.length(); i++){
+        if(blankLine.charAt(i)==' '){
+            words.add(currentStrin);
+            currentStrin = "";
+        }
+        else{
+         currentStrin = currentStrin + blankLine.charAt(i);
+            if(i == blankLine.length()-1)
+            words.add(currentStrin);
+        }
+    }
+    words.sort(String::compareTo);
+    return words;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        for (int i=0;i<str.length())
+        String string = in.nextLine();
+        List<String> sortedWords = get_words(blank_line(string));
+        System.err.println(sortedWords);
+        System.out.println(sortedWords.size());
+
     }
 }
